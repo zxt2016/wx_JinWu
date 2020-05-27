ecb32372aa6ea4689ffd1d96fa74cecc354ecbda
zhangxutong <zhangxutong@meijiexia.com>
2020-05-27 23:28:42 +0800

    zxt

--- pages/index/index.js
+++ pages/index/index.js
@@ -28,21 +28,46 @@ Page({
     starData:[
       {
         num:0,
+        width:'70',
+        height:'70',
+        translateX:'',
+        translateY:''
       },
       {
         num:1,
+        width:'70',
+        height:'70',
+        translateX:'',
+        translateY:''
       },
       {
         num:2,
+        width:'70',
+        height:'70',
+        translateX:'',
+        translateY:''
       },
       {
         num:3,
+        width:'70',
+        height:'70',
+        translateX:'',
+        translateY:''
       },
       {
         num:4,
+        width:'70',
+        height:'70',
+        translateX:'',
+        translateY:''
       },
     ],
-    animation: {},
+    //平均角度
+    deg:'',
+    translateX:'',
+    translateY:'',
+    width:'',
+    height:''
   },
   //事件处理函数
   bindViewTap: function() { 
@@ -51,6 +76,7 @@ Page({
     })
   },
   onLoad: function () { 
+    var _this = this;
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -78,65 +104,196 @@ Page({
       })
     }
     this.getBannerList();
+
+
   },
   onShow(){
     var _this = this;
-    // setInterval(function(){
-    //   _this.starRotate();
-    // },3000);
-  },
+    //星球旋转特效
+    
 
 
-
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
-
-  getBannerList: function() { 
-    var _this = this
-    wx.request({
-      url: app.globalData.baseUrl+'/advertising/getBannertList',
-      method: 'GET', 
-      header: {
-        'content-type': 'application/json'
-      },
-      success (res) {
-        var img_list = []
-        for (var i = 0; i < res.data.data.length; i++) {
-          img_list.push(res.data.data[i].img,)
-        }  
-        _this.setData({
-          banner_img: img_list
-        })
-      }
-    })
-  },
-  //电站切换
-  stationTab: function(e){
-    this.setData({
-      stationType:e.currentTarget.dataset.num
-    });
-  },
-  //星系转动-->从右向左
-  starRotate:function(){
-    var _this = this,
-    starData = _this.data.starData
-    //设定最大值为5，
-    for(var i=0;i<starData.length;i++){
-      if(starData[i].num == 0){
-        starData[i].num = starData.length-1;
-      }else{
-        var val = starData[i].num;
-        starData[i].num = val-1;
-      }
+    const query = wx.createSelectorQuery()
+    var data = _this.data.starData;
+    var deg = 360/data.length;
+    for(var i=0;i<data.length;i++){
+      query.select('.star'+i).boundingClientRect()
     }
-    _this.setData({starData});
-  },
+      query.exec(function(res){
+        console.log(res);
+        for(var n=0;n<res.length;n++){
+//           X坐标=a + Math.sin(角度 * (Math.PI / 180)) * r ；
+// Y坐标=b + Math.cos(角度 * (Math.PI / 180)) * r ； 
+          var x_position = -20 + Math.sin(deg*n* (Math.PI / 180))*130;
+          var z_position = 0 + Math.cos(deg*n* (Math.PI / 180))*130;
+          console.log(x_position,z_position)
+          data[n].translateX = x_position;
+          data[n].translateZ = z_position;
+          
+          
+        }
+        _this.setData({
+            starData:data
+        });
+        var json ={
+          r:140,
+          x0:-10,
+          y0:-40,
 
-  
+          width:res[0].width,
+          height:res[0].height,
+          a:360/_this.data.starData.length,
+          num:0,
+          max:10
+      }
+      _this.threeCircleMove(json);
+      // _this.threeCircleMove(json1);
+      })
+      //每个点角度为72 
+      // X坐标=a + Math.sin(角度 * (Math.PI / 180)) * r
+      // Y坐标=b + Math.cos(角度 * (Math.PI / 180)) * r
+      
+    
+        
+
+
+  },
+  threeCircleMove(json){
+    var _this = this;
+    //要操作的元素
+    var obj = {};
+    //方向(顺时针'+'或逆时针'-')
+    var dir = json.dir;
+    var dir = dir || '+';
+    //最大圈数
+    var max = json.max;
+    max = Number(max) || 'all'; 
+    //半径
+    var r = json.r;
+    r = Number(r) || 100;
+    //圆心x轴坐标
+    var x0 = json.x0;
+    var y0 = json.y0;
+    //元素的初始宽高
+    var offsetHeight =json.height;
+    var offsetWidth =json.width;
+    //元素的宽高
+    var height,width;
+    //初始夹角，以角度为单位
+    var a0 = json.a0;
+    a0 = json.a;
+    
+    //当前夹角
+    var a = a0;
+    //当前圈数
+    var num = json.num || 0;
+    //清除定时器
+    // if(obj.timer){return;}
+    //声明当前值cur
+    var cur = {};
+    var data = _this.data.starData;
+    var timer = new Array();
+    var timer= setInterval(function(){
+      //将这些瞬时值储存在obj对象中的属性中
+      obj.a = a;
+      obj.x0 = x0;
+      obj.width = width;
+      obj.height = height;
+      obj.x = x;
+      obj.num = num;
+      //如果元素运动到指定圈数则停止定时器
+      if(num == max){
+          clearInterval(timer);
+      }
+      //顺时针
+      if(dir == '+'){
+          a++;
+          if(a == a0 + 360){
+              a = a0;
+              num++;
+          }
+      //逆时针
+      }else{
+          a--;
+          if(a == a0 - 360){
+              a = a0;
+              num++;
+          }
+      }
+
+    
+      // for(var i=0;i<data.length;i++){
+      //   var x = data[i].translateX + x0 + r*Math.cos((90 + a*Math.PI)/180);
+      //   var z = y0 + r*Math.sin((90 + a*Math.PI)/180)/4;
+      //   width = (offsetWidth/2) + offsetWidth/2*Math.sin((90 + a*Math.PI)/180);
+      //   data[i].translateX = x;
+      //   _this.setData({
+      //     starData:data
+      //   });
+      //   // height = (offsetHeight/2) + offsetWidth/2*Math.sin((90 + a*Math.PI)/180);
+      // }
+      
+      // _this.setData({
+      //   translateX:x,
+      //   translateY:y,
+      //   width:width,
+      //   height:height
+      // });
+      
+  },3000);
+    
+    
+    
+},
+
+
+getUserInfo: function(e) {
+  console.log(e)
+  app.globalData.userInfo = e.detail.userInfo
+  this.setData({
+    userInfo: e.detail.userInfo,
+    hasUserInfo: true
+  })
+},
+
+getBannerList: function() { 
+  var _this = this
+  wx.request({
+    url: app.globalData.baseUrl+'/advertising/getBannertList',
+    method: 'GET', 
+    header: {
+      'content-type': 'application/json'
+    },
+    success (res) {
+      var img_list = []
+      for (var i = 0; i < res.data.data.length; i++) {
+        img_list.push(res.data.data[i].img,)
+      }  
+      _this.setData({
+        banner_img: img_list
+      })
+    }
+  })
+},
+//电站切换
+stationTab: function(e){
+  this.setData({
+    stationType:e.currentTarget.dataset.num
+  });
+},
+//星系转动-->从右向左
+starRotate:function(){
+  var _this = this,
+  starData = _this.data.starData
+  //设定最大值为5，
+  for(var i=0;i<starData.length;i++){
+    if(starData[i].num == 0){
+      starData[i].num = starData.length-1;
+    }else{
+      var val = starData[i].num;
+      starData[i].num = val-1;
+    }
+  }
+  _this.setData({starData});
+},
 })
