a6a9b40b4fe27f5d35afc375caf178697c4f37f2
zhangxutong <zhangxutong@meijiexia.com>
2020-05-27 10:19:33 +0800

    zxt

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,6 @@
 //index.js
 //获取应用实例
 const app = getApp();
-// var THREE = require('../../utils/three');
-// var OrbitControls = require('../../utils/OrbitControls');
-// import { OrbitControls } from '../../utils/gltf-loader'
 Page({
   data: {
     motto: 'Hello World',
@@ -22,7 +19,6 @@ Page({
     autoplay2: false,
     circular2: true,
     indicatorDots2: false,
-
 
     //电站tab切换标识
     stationType:1,
@@ -82,95 +78,7 @@ Page({
       })
     }
     this.getBannerList();
-
-
-    // wx.createSelectorQuery()
-    //   .select('#c')
-    //   .node()
-    //   .exec((res) => {
-    //     const canvas = THREE.global.registerCanvas(res[0].node)
-    //     // const canvas = res[0].node
-    //     this.setData({ canvasId: canvas._canvasId })
-
-    //     const camera = new THREE.PerspectiveCamera(70, canvas.width / canvas.height, 1, 1000);
-    //     camera.position.z = 500;
-    //     const scene = new THREE.Scene();
-    //     scene.background = new THREE.Color(0xAAAAAA);
-    //     const renderer = new THREE.WebGLRenderer({ antialias: true });
-      
-    //     const controls = new OrbitControls(camera, renderer.domElement);
-    //     // controls.enableDamping = true;
-    //     // controls.dampingFactor = 0.25;
-    //     // controls.enableZoom = false;
-    //     camera.position.set(200, 200, 500);
-    //     controls.update();
-    //     const geometry = new THREE.BoxBufferGeometry(200, 200, 200);
-      
-    //     const texture = new THREE.TextureLoader().load('./pikachu.png');
-    //     const material = new THREE.MeshBasicMaterial({ map: texture });
-      
-    //     // const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
-    //     const mesh = new THREE.Mesh(geometry, material);
-    //     scene.add(mesh);
-      
-    //     // renderer.setPixelRatio(wx.getSystemInfoSync().pixelRatio);
-    //     // renderer.setSize(canvas.width, canvas.height);
-      
-    //     function onWindowResize() {
-    //       camera.aspect = window.innerWidth / window.innerHeight;
-    //       camera.updateProjectionMatrix();
-    //       renderer.setSize(canvas.width, canvas.height);
-    //     }
-    //     function render() {
-    //       canvas.requestAnimationFrame(render);
-    //       // mesh.rotation.x += 0.005;
-    //       // mesh.rotation.y += 0.01;
-    //       controls.update();
-    //       renderer.render(scene, camera);
-    //     }
-
-    //     render()
-
-    //   })
   },
-  onUnload: function () {
-    THREE.global.unregisterCanvas(this.data.canvasId)
-  },
-  touchStart(e) {
-    console.log('canvas', e)
-    THREE.global.touchEventHandlerFactory('canvas', 'touchstart')(e)
-  },
-  touchMove(e) {
-    console.log('canvas', e)
-    THREE.global.touchEventHandlerFactory('canvas', 'touchmove')(e)
-  },
-  touchEnd(e) {
-    console.log('canvas', e)
-    THREE.global.touchEventHandlerFactory('canvas', 'touchend')(e)
-  },
-  touchCancel(e) {
-    // console.log('canvas', e)
-  },
-  longTap(e) {
-    // console.log('canvas', e)
-  },
-  tap(e) {
-    // console.log('canvas', e)
-  },
-  documentTouchStart(e) {
-    // console.log('document',e)
-  },
-  documentTouchMove(e) {
-    // console.log('document',e)
-  },
-  documentTouchEnd(e) {
-    // console.log('document',e)
-  },
-
-
-
-
-
   onShow(){
     var _this = this;
     // setInterval(function(){
