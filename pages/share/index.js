2ac9ea98e66c7fe9acad55010532474bb3b3bd80
zhangruixue-sherry <919194523@qq.com>
2020-05-29 20:44:22 +0800

    修改

--- pages/share/index.js
+++ pages/share/index.js
@@ -5,10 +5,29 @@ Page({
   data: {
     imgUrl:app.globalData.imgUrl,
     token:wx.getStorageSync('token'),
+    dataInfo:{}
   },
   onLoad: function () {
     
-    
+    this.dataList();
   },
+  dataList: function() { 
+    var _this = this
+    wx.request({
+      url: app.globalData.baseUrl+'/wxUser/userCompanyInfo',
+      method: 'GET', 
+      header: {
+        'content-type': 'application/json',
+        'authorization': app.globalData.token,
+        'openId': '1'
+      },
+      success (res) {
+        console.log(res) 
+        _this.setData({
+          dataInfo: res.data.data.wxCompanyList
+        })
+      }
+    })
+  }
  
 })
