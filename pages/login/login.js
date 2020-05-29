1ae064305750cd0770dc5a1fe07ec9cad4bcbbaa
zhangxutong <zhangxutong@meijiexia.com>
2020-05-29 13:32:14 +0800

    11

--- pages/login/login.js
+++ pages/login/login.js
@@ -36,19 +36,19 @@ Component({
         wx.showToast({
           title: '请输入手机号',
           icon:'none',
-          duration: 2000
+          duration: 3000
         })
       } else if (/^(13[0-9]|14[5-9]|15[012356789]|16[0-9]|17[0-8]|18[0-9]|19[8-9])[0-9]{8}$/.test(phone) != true){
         wx.showToast({
           title: '手机号格式不正确',
           icon: 'none',
-          duration: 2000
+          duration: 3000
         })
       }else if(!code){
         wx.showToast({
           title: '请输入验证码',
           icon: 'none',
-          duration: 2000
+          duration: 3000
         })
       }else{
         console.log(JSON.stringify({
@@ -91,20 +91,19 @@ Component({
     },
     changeVal(e){
       var text = e.currentTarget.dataset.type;
-      console.log(text);
       var input_val = e.detail.value;
       if(text == 'phone'){
         if(!input_val){
           wx.showToast({
             title: '请输入手机号',
             icon:'none',
-            duration: 2000
+            duration: 3000
           })
         } else if (/^(13[0-9]|14[5-9]|15[012356789]|16[0-9]|17[0-8]|18[0-9]|19[8-9])[0-9]{8}$/.test(input_val) != true){
           wx.showToast({
             title: '手机号格式不正确',
             icon: 'none',
-            duration: 2000
+            duration: 3000
           })
         }else{
             this.setData({
@@ -116,7 +115,7 @@ Component({
           wx.showToast({
             title: '请输入短信验证码',
             icon:'none',
-            duration: 2000
+            duration: 3000
           })
         }
       }
@@ -125,57 +124,79 @@ Component({
     //获取验证码-倒计时
     sendMessage() {
       var _this = this;
-      //设置button效果，开始计时
-      _this.setData({
-        tip:false,
-        curCount:_this.data.count,
-      });
-      var times = setInterval(function(){
-        var time = _this.data.curCount;
-      
-        if (time == 0) {
-            clearInterval(times);//停止计时器
-            _this.setData({
-              tip:true
-            });
-        }else {
-          time--;
-          _this.setData({
-            curCount:time
-          });
-        }
-      }, 1000)
-      console.log(_this.data.phone);
-      //向后台发送处理数据
-      wx.request({
-        url: app.globalData.baseUrl+'/sms/send',
-        method:'POST',
-        dataType:'json',
-        header:{
-          'Authorization':app.globalData.token,
-          'content-type': 'application/json'
-        },
-        data:{
-          phone:_this.data.phone,
-        },
-        success(res){
-          console.log(res);
-          if(res.statusCode == 200){
-            wx.showToast({
-              title: '验证码发送成功',
-              icon:'none',
-              duration: 2000
-            })
-          }else{
-            wx.showToast({
-              title: '验证码发送失败',
-              icon:'none',
-              duration: 2000
-            })
-          }
-        },
-      })
-      
+      if(!_this.data.phone){
+        wx.showToast({
+          title: '请输入手机号',
+          icon:'none',
+          duration: 3000
+        })
+      } else if (/^(13[0-9]|14[5-9]|15[012356789]|16[0-9]|17[0-8]|18[0-9]|19[8-9])[0-9]{8}$/.test(_this.data.phone) != true){
+        wx.showToast({
+          title: '手机号格式不正确',
+          icon: 'none',
+          duration: 3000
+        })
+      }else{
+        //向后台发送处理数据
+        wx.request({
+          url: app.globalData.baseUrl+'/sms/send',
+          method:'POST',
+          dataType:'json',
+          header:{
+            'Authorization':app.globalData.token,
+            'content-type': 'application/json'
+          },
+          data:{
+            phone:_this.data.phone,
+          },
+          success(res){
+            console.log(res);
+            if(res.data.errcode == 0){
+              //设置button效果，开始计时
+              _this.setData({
+                tip:false,
+                curCount:_this.data.count,
+              });
+              var times = setInterval(function(){
+                var time = _this.data.curCount;
+              
+                if (time == 0) {
+                    clearInterval(times);//停止计时器
+                    _this.setData({
+                      tip:true
+                    });
+                }else {
+                  time--;
+                  _this.setData({
+                    curCount:time
+                  });
+                }
+              }, 1000)
+              wx.showToast({
+                title: '验证码发送成功',
+                icon:'none',
+                duration: 3000
+              })
+            }else{
+              console.log(111);
+              if(res.data.errcode == 30001){
+                wx.showToast({
+                  title: '次数已达上限,请30分钟后操作',
+                  icon:'none',
+                  duration: 3000
+                })
+              }else{
+                wx.showToast({
+                  title: res.data.errmsg,
+                  icon:'none',
+                  duration: 3000
+                })
+              }
+              
+            }
+          },
+        })
+      }
     },
   
   
